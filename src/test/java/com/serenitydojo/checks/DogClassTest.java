af58f8cc60fb10f39426ee4bce8f06f096929dfe
ACXGREZ <71022776+ACXGREZ@users.noreply.github.com>
2023-04-27 20:18:19 -0400

    Exercise 3

--- src/test/java/com/serenitydojo/checks/DogClassTest.java
+++ src/test/java/com/serenitydojo/checks/DogClassTest.java
@@ -1,10 +1,20 @@
 package com.serenitydojo.checks;
 
+import com.serenitydojo.Dog;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DogClassTest {
+
+    @Test
+    public void creating_a_dog() {
+        Dog fido = new Dog("Fido","Bone", 5);
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
+    }
     @Test
     public void theDogClassShouldExist() throws ClassNotFoundException {
         assertThat(Class.forName("com.serenitydojo.Dog")).isNotNull();
